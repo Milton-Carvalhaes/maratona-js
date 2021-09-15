c19eaa0a6e38463d51f7d52ee14af5f25e397d9c
Milton-Carvalhaes <milton.carvalhaes40@gmail.com>
2021-09-15 03:07:59 -0300

    Styling this bad boy

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="valid-keys">
+        <span className="matched">mariate</span>
+        <span className="remainder">gui</span>
+      </div>
+      <div className="typed-keys">
+        akusuhdlahdh
+      </div>
+      <div className="completed-words">
+        <ol>
+          <li>cidade</li>
+          <li>vampeta</li>
+          <li>neto</li>
+        </ol>
+      </div>
+    </div>
+
+  )
+}
+
+export default App;
